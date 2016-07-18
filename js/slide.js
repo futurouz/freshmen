08b7e372b97d1aa7fb3187a85075743e358c5014
Kunanan Tassuwan <Kunanan Tassuwan>
2016-07-19 00:50:35 +0700

    add smooth slide

---
+++ js/slide.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function(){
+  $("#edu").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#eduSlide").offset().top
+        }, 1000);
+      });
+  $("#ser").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#serSlide").offset().top
+          }, 1000);
+      });
+  $("#per").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#perSlide").offset().top
+          }, 1000);
+      });
+  $("#rule").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#ruleSlide").offset().top
+          }, 1000);
+      });
+  $("#map").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#mapSlide").offset().top
+          }, 1000);
+      });
+  $("#shirt").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#shirtSlide").offset().top
+          }, 1000);
+      });
+  $("#secur").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#securSlide").offset().top
+            }, 1000);
+      });
+  $("#acti").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#actiSlide").offset().top
+            }, 1000);
+      });
+  $("#car").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#carSlide").offset().top
+            }, 1000);
+      });
+  });
