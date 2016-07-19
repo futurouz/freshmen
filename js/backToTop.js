0e314e3a3553429e9b3e9e544f63aa70db21a884
Kunanan Tassuwan <Kunanan Tassuwan>
2016-07-20 00:37:11 +0700

    add button to Top

---
+++ js/backToTop.js
@@ -0,0 +1,17 @@
+$(function(){
+
+  //Scroll event
+  $(window).scroll(function(){
+    var scrolled = $(window).scrollTop();
+    if (scrolled > 1000) $('.goToTop').fadeIn('slow');
+    if (scrolled < 1000) $('.goToTop').fadeOut('slow');
+  });
+
+  //Click event
+  $('.goToTop').click(function () {
+    $('html, body').animate({
+        scrollTop: $("#ruleSlide").offset().top
+      }, 1000);
+    });
+
+});
